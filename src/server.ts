280b55436f79fadd1531f8ea1b8acae4baaf5ee6
Alan Brazões <alanbrazoes@gmail.com>
2022-03-16 11:45:27 -0300

    helmet

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import cors from 'cors';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 
 import routes from '@router';
 
@@ -13,7 +13,7 @@ mongoose
   .catch((e) => app.emit(e));
 
 app.use(cors());
-// app.use(helmet());
+app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
