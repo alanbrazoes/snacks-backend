a80a0490dc7edee015897b1891b335d516354eb4
Alan Brazões <alanbrazoes@gmail.com>
2022-05-22 19:12:56 -0300

    feat: add procfile

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,32 @@
 import express from 'express';
-import 'dotenv/config';
-import mongoose from 'mongoose';
+import { Request, Response } from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
+import 'dotenv/config';
+// import mongoose from 'mongoose';
+// import helmet from 'helmet';
 
 // import routes from '@router';
-import { error } from '@middlewares/error';
-import { Request, Response } from 'express';
+// import { error } from '@middlewares/error';
 
 const app = express();
-mongoose
-  .connect(process.env.URLSERVER as string)
-  .then(() => app.emit('ok'))
-  .catch((err) => console.log(err));
-
+app.use(express.json());
 app.use(cors());
-app.use(helmet());
+
+// mongoose
+//   .connect(process.env.URLSERVER as string)
+//   .then(() => app.emit('ok'))
+//   .catch((err) => console.log(err));
+
+// app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
   res.status(200).json({ response: 'ok' });
 });
 
 // app.use(routes);
-app.use(error);
+// app.use(error);
 
 app.on('ok', () => {
   app.listen(process.env.PORT || process.env.LISTEN);
