5b44b4d502a023da66e014e7b9c27695195e7723
Alan Brazões <alanbrazoes@gmail.com>
2022-05-22 19:21:13 -0300

    feat: add procfile

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,22 @@ import express from 'express';
 import { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-// import mongoose from 'mongoose';
-// import helmet from 'helmet';
+import mongoose from 'mongoose';
+import helmet from 'helmet';
 
-// import routes from '@router';
-// import { error } from '@middlewares/error';
+import routes from '@router';
+import { error } from '@middlewares/error';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// mongoose
-//   .connect(process.env.URLSERVER as string)
-//   .then(() => app.emit('ok'))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(process.env.URLSERVER as string)
+  .then(() => app.emit('ok'))
+  .catch((err) => console.log(err));
 
-// app.use(helmet());
+app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,9 +25,9 @@ app.get('/', (_req: Request, res: Response) => {
   res.status(200).json({ response: 'ok' });
 });
 
-// app.use(routes);
-// app.use(error);
+app.use(routes);
+app.use(error);
 
-app.listen(process.env.PORT || 3333);
-// app.on('ok', () => {
-// });
+app.on('ok', () => {
+  app.listen(3333);
+});
