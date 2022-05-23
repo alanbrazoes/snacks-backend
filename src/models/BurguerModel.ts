81b5ef4aadc938bf2727a4ca1f9de7ea781387d2
Alan Brazões <alanbrazoes@gmail.com>
2022-05-23 10:20:19 -0300

    feat: add field required in burguer model

--- src/models/BurguerModel.ts
+++ src/models/BurguerModel.ts
@@ -1,11 +1,26 @@
 import mongoose from 'mongoose';
 
 const BurguerSchema = new mongoose.Schema({
-  name: String,
-  preparationTime: Number,
-  ingredients: Array,
-  price: Number,
-  type: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  preparationTime: {
+    type: Number,
+    required: true,
+  },
+  ingredients: {
+    type: Array,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
 });
 
 const BurguerModel = mongoose.model('hamburguers', BurguerSchema);
