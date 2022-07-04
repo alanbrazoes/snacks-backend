8ff4a7f4e093da2e82beeaa54c068f82a966a921
Alan Brazões <alanbrazoes@gmail.com>
2022-07-04 12:55:31 -0300

    refactor: return get all snacks

--- src/services/allSnacks.services.ts
+++ src/services/allSnacks.services.ts
@@ -3,16 +3,18 @@ import { BurguerModel, DishModel, DrinkModel } from '@models/index';
 const models = [BurguerModel, DishModel, DrinkModel];
 
 const getAllSnacks = async () => {
+  let snacks = {};
+
   const getSnacks = models.map(async (model) => {
     try {
       const data = await model.find();
-      return { [model.modelName]: data };
+      snacks = { ...snacks, [model.modelName]: data };
     } catch (error) {
       return [];
     }
   });
 
-  const snacks = await Promise.all(getSnacks);
+  await Promise.all(getSnacks);
   return snacks;
 };
 
